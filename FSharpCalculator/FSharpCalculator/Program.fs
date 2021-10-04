8ead146e77244ca1336e56d8c15ee7d45652c6fd
Mattias Eriksson <mattias.eriksson@hogia.se>
2021-10-04 17:57:44 +0200

    added fibonaci

--- FSharpCalculator/FSharpCalculator/Program.fs
+++ FSharpCalculator/FSharpCalculator/Program.fs
@@ -4,5 +4,17 @@ open System
 
 [<EntryPoint>]
 let main argv =
+    let n = Console.ReadLine()
+    let mutable i = 1
+    let mutable j = 1
+    printfn "%i" i
+    printfn "%i" j
+    for k in 1 .. int(n) do 
+        let c = i + j
+        printfn "%i" c
+        i <- j 
+        j <- c
+
+
     printfn "Hello Hacktoberfest!"
     0 // return an integer exit code
